5189654f28ae3abea10e654425ed75f442be5167
al-anas <alanas7171@gmail.com>
2022-03-02 13:42:16 +0600

    display search result added

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+/* search field */
 const searchField=()=>{
     const searchField=document.getElementById('search-field');
     const searchText=searchField.value;
-    console.log(searchText);
     searchField.value='';
+    const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(json => displaySearchResult(json.data.slice(0,20)));
+}
+
+/* display search result */
+
+const displaySearchResult= phones =>{
+    const phonesArea = document.getElementById('phones-area');
+    phonesArea.textContent='';
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML =`
+            <div class="card">
+             <img src="${phone.image}" class="card-img-top" alt="...">
+             <div class="card-body text-center">
+                <h3 class="card-title my-2">${phone.phone_name}</h3>
+                <h4 class="my-3">${phone.brand}</h4>
+                <button type="button" class="btn btn-primary">See Detail</button>
+             </div>
+            </div>`;
+        phonesArea.appendChild(div);
+    })
+
 }
