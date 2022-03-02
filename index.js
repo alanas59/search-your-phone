1ba3204fe03e86bede7730963eaf9b95e964f709
al-anas <alanas7171@gmail.com>
2022-03-02 13:00:39 +0600

    search field function added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const searchField=()=>{
-    console.log("ok");
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    console.log(searchText);
+    searchField.value='';
 }
